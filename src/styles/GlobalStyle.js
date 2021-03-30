465c656590725d8add101945c01d9394f6bf6c33
iamsonghee <65877281+iamsonghee@users.noreply.github.com>
2021-03-30 20:07:16 +0900

    MODIFY : 초기세팅변경 스타일컴포넌트 작업환경 구성 및 eslint 에러방지

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset"; // style-reset 패키지
+
+const GlobalStyle = createGlobalStyle` 
+    ${reset}
+    @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@400;500;700&display=swap');
+    a{
+        text-decoration: none;
+        color: inherit;
+    }
+    *{
+        box-sizing: border-box;
+    }
+    body {
+        font-family: 'Noto Sans KR', sans-serif !important;
+        font-size: 14px;
+    }
+`;
+
+export default GlobalStyle;
