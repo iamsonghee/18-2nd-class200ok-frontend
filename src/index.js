76427e4213b862233c26c6076f1a97f6725cfde9
iamsonghee <65877281+iamsonghee@users.noreply.github.com>
2021-03-31 16:18:22 +0900

    ADD : Lecture(강의)컴포넌트 레이아웃

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Routes from "./Routes";
-// import "./styles/common.scss";
-// import "./styles/reset.scss";
 import GlobalStyle from "./styles/GlobalStyle";
 
 ReactDOM.render(
